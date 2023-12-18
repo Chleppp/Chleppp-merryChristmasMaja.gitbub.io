33f21b325c6648142a820ed3245c345c588ded86
Chleppp <99537212+Chleppp@users.noreply.github.com>
2023-12-19 00:26:44 +0100

    Add files via upload

---
+++ java.js
@@ -0,0 +1,45 @@
+function dodgeDefault() {
+    var element = document.getElementById("pdfObject");
+    document.body.style.backgroundColor = "#0a0614";
+    element.setAttribute("data", "dchfiolet.pdf");
+
+    var but1 = document.getElementById("1");
+    but1.style.backgroundColor = "#bba6d9";
+    but1.style.color = "#ebe2f7";
+
+    var but2 = document.getElementById("2");
+    but2.style.backgroundColor = "#bba6d9";
+    but2.style.color = "#ebe2f7";
+
+    var but3 = document.getElementById("3");
+    but3.style.backgroundColor = "#bba6d9";
+    but3.style.color = "#ebe2f7";
+
+    var butbg = document.getElementById("buttonBg");
+    butbg.style.backgroundColor = "#362748";
+  }
+  function dodgeWhite() {}
+  function kitty() {
+    var element = document.getElementById("pdfObject");
+    document.body.style.backgroundColor = "#10121f";
+    element.setAttribute("data", "dc300.pdf");
+
+    var but1 = document.getElementById("1");
+    but1.style.backgroundColor = "#85a9e2";
+    but1.style.color = "#dee9fb";
+
+    var but2 = document.getElementById("2");
+    but2.style.backgroundColor = "#85a9e2";
+    but2.style.color = "#dee9fb";
+
+    var but3 = document.getElementById("3");
+    but3.style.backgroundColor = "#85a9e2";
+    but3.style.color = "#dee9fb";
+
+    var butbg = document.getElementById("buttonBg");
+    butbg.style.backgroundColor = "#1f2130";
+  }
+  function other() {
+    window.location.href =
+      "https://drive.google.com/drive/folders/1Q4nVRVhdOqCJHnvhMpmI0ANaeEVC6XPK?usp=sharing";
+  }
