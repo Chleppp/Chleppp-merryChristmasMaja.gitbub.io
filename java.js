a18634109183f54c14aaeae72fcaf2c46ddf5d24
Chleppp <99537212+Chleppp@users.noreply.github.com>
2024-03-28 00:43:56 +0100

    Update java.js

--- java.js
+++ java.js
@@ -1,44 +1,46 @@
 function dodgeDefault() {
-    var element = document.getElementById("pdfObject");
-    document.body.style.backgroundColor = "#0a0614";
-    element.setAttribute("src", "https://drive.google.com/file/d/1rub3psjtfpeiPpygxYl5ivxWLCXuhDo5/preview?usp=drive_link");
+  var element = document.getElementById("pdfObject");
+  document.body.style.backgroundColor = "#0a0614";
+  element.setAttribute("src", "https://drive.google.com/file/d/1rub3psjtfpeiPpygxYl5ivxWLCXuhDo5/preview?usp=drive_link");
 
-    var but1 = document.getElementById("1");
-    but1.style.backgroundColor = "#bba6d9";
-    but1.style.color = "#ebe2f7";
+  var but1 = document.getElementById("1");
+  but1.style.backgroundColor = "#bba6d9";
+  but1.style.color = "#ebe2f7";
 
-    var but2 = document.getElementById("2");
-    but2.style.backgroundColor = "#bba6d9";
-    but2.style.color = "#ebe2f7";
+  var but2 = document.getElementById("2");
+  but2.style.backgroundColor = "#bba6d9";
+  but2.style.color = "#ebe2f7";
 
-    var but3 = document.getElementById("3");
-    but3.style.backgroundColor = "#bba6d9";
-    but3.style.color = "#ebe2f7";
+  var but3 = document.getElementById("3");
+  but3.style.backgroundColor = "#bba6d9";
+  but3.style.color = "#ebe2f7";
 
-    var butbg = document.getElementById("buttonBg");
-    butbg.style.backgroundColor = "#362748";
-  }
-  function kitty() {
-    var element = document.getElementById("pdfObject");
-    document.body.style.backgroundColor = "#10121f";
-    element.setAttribute("src", "https://drive.google.com/file/d/1B4K1JE4jiffAVnTSYeWE-MEZOKyhSmWx/preview?usp=drive_link");
+  var but4 = document.getElementById("3");
 
-    var but1 = document.getElementById("1");
-    but1.style.backgroundColor = "#85a9e2";
-    but1.style.color = "#dee9fb";
+  var butbg = document.getElementById("buttonBg");
+  butbg.style.backgroundColor = "#362748";
+}
+function kitty() {
+  var element = document.getElementById("pdfObject");
+  document.body.style.backgroundColor = "#10121f";
+  element.setAttribute("src", "https://drive.google.com/file/d/1B4K1JE4jiffAVnTSYeWE-MEZOKyhSmWx/preview?usp=drive_link");
 
-    var but2 = document.getElementById("2");
-    but2.style.backgroundColor = "#85a9e2";
-    but2.style.color = "#dee9fb";
+  var but1 = document.getElementById("1");
+  but1.style.backgroundColor = "#85a9e2";
+  but1.style.color = "#dee9fb";
 
-    var but3 = document.getElementById("3");
-    but3.style.backgroundColor = "#85a9e2";
-    but3.style.color = "#dee9fb";
+  var but2 = document.getElementById("2");
+  but2.style.backgroundColor = "#85a9e2";
+  but2.style.color = "#dee9fb";
 
-    var butbg = document.getElementById("buttonBg");
-    butbg.style.backgroundColor = "#1f2130";
-  }
-  function other() {
-    window.location.href =
-      "https://drive.google.com/drive/folders/1Q4nVRVhdOqCJHnvhMpmI0ANaeEVC6XPK?usp=sharing";
-  }
+  var but3 = document.getElementById("3");
+  but3.style.backgroundColor = "#85a9e2";
+  but3.style.color = "#dee9fb";
+
+  var butbg = document.getElementById("buttonBg");
+  butbg.style.backgroundColor = "#1f2130";
+}
+function other() {
+  window.location.href =
+    "https://drive.google.com/drive/folders/1Q4nVRVhdOqCJHnvhMpmI0ANaeEVC6XPK?usp=sharing";
+}
